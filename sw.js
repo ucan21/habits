b9789c3060e26c04759dd8ced0235a11f110d35b
ucan21 <127005238+ucan21@users.noreply.github.com>
2026-04-23 07:41:10 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const C='henry-v7';const F=['./','./index.html','./manifest.json'];
+const C='henry-v9-fresh';
+const F=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(F)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
