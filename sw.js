5419708dff49fe3ffe90a2be6bb5235f02ec58f3
ucan21 <127005238+ucan21@users.noreply.github.com>
2026-04-24 12:01:01 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,28 @@
-const C='henry-v9-fresh';
-const F=['./','./index.html','./franklin.html','./manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(F)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+const C='henry-v10';
+const F=[
+  './',
+  './index.html',
+  './habits.html',
+  './brain.html',
+  './franklin.html',
+  './gratitude.html',
+  './gcal.html',
+  './manifest.json'
+];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(C).then(c=>c.addAll(F)));
+  self.skipWaiting();
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(ks=>Promise.all(
+      ks.filter(k=>k!==C).map(k=>caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
+});
