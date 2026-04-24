d37c1f9f56e58eb05a80886e473e429e60b591fd
ucan21 <127005238+ucan21@users.noreply.github.com>
2026-04-24 15:26:40 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='henry-diary-v4';
+const C='henry-diary-v5';
 const F=[
   './',
   './index.html',
@@ -10,20 +10,6 @@ const F=[
   './gtask.html',
   './manifest.json'
 ];
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(C).then(c=>c.addAll(F)));
-  self.skipWaiting();
-});
-self.addEventListener('activate',e=>{
-  e.waitUntil(
-    caches.keys().then(ks=>Promise.all(
-      ks.filter(k=>k!==C).map(k=>caches.delete(k))
-    ))
-  );
-  self.clients.claim();
-});
-self.addEventListener('fetch',e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
-  );
-});
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(F)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
