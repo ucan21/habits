ea2c8ea5fe9df68d95c349c764d0c6c18c35df88
ucan21 <127005238+ucan21@users.noreply.github.com>
2026-04-26 20:28:26 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,15 @@
-const C='henry-invest-v1';
-const F=['./', './index.html','./portfolio.html','./dividend.html','./journal.html','./checklist.html','./sheets.html','./manifest.json'];
+const C='henry-diary-v28';
+const F=[
+  './',
+  './index.html',
+  './habits.html',
+  './brain.html',
+  './gtask.html',
+  './gratitude.html',
+  './gcal.html',
+  './backup.html',
+  './manifest.json'
+];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(F)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(caches.open(C).then(cache=>cache.match(e.request).then(r=>r||fetch(e.request).then(res=>{cache.put(e.request,res.clone());return res;}))));});
