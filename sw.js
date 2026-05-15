9e29b75c3f2dc7259965ce46c850d2e6d663a931
ucan21 <127005238+ucan21@users.noreply.github.com>
2026-05-15 15:01:16 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='hd-v115-store';
+const C='hd-v116-fix';
 const F=['/habits/','/habits/index.html','/habits/habits.html','/habits/brain.html','/habits/gtask.html','/habits/gratitude.html','/habits/gcal.html','/habits/backup.html','/habits/manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(F)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k)))));self.clients.claim();});
